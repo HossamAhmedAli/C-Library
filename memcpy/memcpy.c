6ddd2c027ae209d83c642f03d4b1064cd485d99b
HossamAhmedAli <56847513+HossamAhmedAli@users.noreply.github.com>
2023-10-17 13:00:11 +0300

    memcpy.c
    memcpy() is used to copy a block of memory from one location to another ..

--- memcpy/memcpy.c
+++ memcpy/memcpy.c
@@ -1,1 +1,13 @@
+void *my_memcpy( void *destination, const void *source, int length)
+{
+    if( destination==NULL || source==NULL ) return NULL;
+    else
+    {
+      while(length--)
+        *((char *)destination+length) = *((char *)source+length);
+    } //end else
+
+
+    return destination;
+}
 
